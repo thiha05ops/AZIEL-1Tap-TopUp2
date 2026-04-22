0c3bc899aeab600268d40659ef4db51c706e0459
Thi ha <thiha@This-MacBook-Pro.local>
2026-04-23 02:21:48 +0700

    add payment screenshot telegram system

--- backend/services/telegram.js
+++ backend/services/telegram.js
@@ -6,7 +6,8 @@ const TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
 async function sendTelegramMessage(text) {
-    const url = `https://api.telegram.org/bot8707178938:AAGTRfi4GeNowgQQmrlLybpkVwVClSYDbkI/sendMessage`;
+    const url = `https://api.telegram.org/bot8667442810: AAGTx8D67UGGBHSOgNc2-7nGdPHmeHMbmds
+/sendMessage`;
 
     await fetch(url, {
         method: "POST",
@@ -14,17 +15,17 @@ async function sendTelegramMessage(text) {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            chat_id: CHAT_ID,
+            chat_id: 8667442810,
             text
         })
     });
 }
 
 async function sendTelegramPhoto(filePath, caption) {
-    const url = `https://api.telegram.org/bot8707178938:AAGTRfi4GeNowgQQmrlLybpkVwVClSYDbkI/sendPhoto`;
+    const url = `https://api.telegram.org/bot${TOKEN}/sendPhoto`;
 
     const form = new FormData();
-    form.append("chat_id", 8707178938);
+    form.append("chat_id", CHAT_ID);
     form.append("caption", caption);
     form.append("photo", fs.createReadStream(filePath));
 
