2af7901ba4eefd7629fd313e3e6c26e510ec0b24
Thi ha <thiha@This-MacBook-Pro.local>
2026-04-26 19:09:17 +0700

    fix google callback url

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -8,7 +8,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "/api/auth/google/callback",
+            callbackURL: "https://aziel-1tap-topup2.onrender.com/api/auth/google/callback",
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
