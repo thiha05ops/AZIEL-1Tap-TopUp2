85896b990234cc2d2bc3c7e6064493116c28a6df
Thi ha <thiha@This-MacBook-Pro.local>
2026-05-02 14:14:26 +0700

    update html

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -60,29 +60,9 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 /* Toast Function */
-t(text) {
+function showToast(text) {
 
     let toast = document.getElementById("siteToast");
-
-    if (!toast) {
-        toast = document.createElement("div");
-        toast.id = "siteToast";
-        document.body.appendChild(toast);
-
-        toast.style.position = "fixed";
-        toast.style.bottom = "25px";
-        toast.style.right = "25px";
-        toast.style.background = "linear-gradient(90deg,#7c3aed,#4f46e5)";
-        toast.style.color = "#fff";
-        toast.style.padding = "14px 18px";
-        toast.style.borderRadius = "14px";
-        toast.style.fontWeight = "700";
-        toast.style.zIndex = "99999";
-        toast.style.boxShadow = "0 12px 28px rgba(0,0,0,.25)";
-        toast.style.opacity = "0";
-        toast.style.transition = ".3s";
-        toast.style.maxWidth = "320px";
-    }
 
     toast.innerText = text;
     toast.style.opacity = "1";
