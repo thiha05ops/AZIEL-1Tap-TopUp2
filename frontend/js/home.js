f3f09d309e6eba211e3621e231032f6fa214b2c2
Thi ha <thiha@This-MacBook-Pro.local>
2026-05-07 20:28:06 +0700

    fix logout conflict

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -30,15 +30,6 @@ document.addEventListener("DOMContentLoaded", () => {
                 dropdown.style.display === "flex" ? "none" : "flex";
         });
     }
-
-    if (logoutBtn) {
-        logoutBtn.addEventListener("click", (e) => {
-            e.stopPropagation();
-            localStorage.clear();
-            window.location.href = "login.html";
-        });
-    }
-
     // toggle dropdown
     profileBox.addEventListener("click", () => {
         dropdown.style.display =
