f239950a9b747457ca54426a5319d4a00e202922
Thi ha <thiha@This-MacBook-Pro.local>
2026-05-21 23:14:40 +0700

    Fix LiveChat case issue

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ const passwordRoutes = require("./routes/password");
 const supplierRoutes = require("./routes/supplier");
 const walletRoutes = require("./routes/wallet");
 const adminStatsRoutes = require("./routes/adminStats");
-const liveChatRoutes = require("./routes/liveChat");
+const liveChatRoutes = require("./routes/LiveChat");
 app.use("/api/live-chat", liveChatRoutes);
 // EXPRESS APP
 const app = express();
